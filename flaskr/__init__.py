108766e05777dd9d436b26cb9ffea4981d908db2
jmacura <jeremiaszmacura99@gmail.com>
2022-10-22 17:00:38 +0200

    fix unit test workflow

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,2 @@
-__version__ = '0.1.0'
-
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 from flaskr import __version__
 
-setup(version=__version__)
+setup(version='0.1.0')
